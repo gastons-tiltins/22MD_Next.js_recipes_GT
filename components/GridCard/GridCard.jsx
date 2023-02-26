97842ccf9f489b2ef8fc5e135889a6b39065ed8a
Gastons Tiltins <gastons.tiltins@gmail.com>
2023-02-26 16:43:25 +0200

    adding dynamic

--- components/GridCard/GridCard.jsx
+++ components/GridCard/GridCard.jsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
 
-export default function GridCard() {
+export default function GridCard({id, name, imageLink, desc, category}) {
     return (
         <div>
             <div className='card'>
                 <div className='card-image'>
                     <figure className='image is-4by3'>
                         <Image
-                            src='/soup.jpeg'
-                            alt='me'
+                            className='aspectRatio'
+                            src={imageLink}
+                            alt={name}
                             // width={1000}
                             // height={1000}
                             fill
@@ -17,26 +18,13 @@ export default function GridCard() {
                 </div>
                 <div className='card-content'>
                     <div className='media'>
-                        <div className='media-left'>
-                            <figure className='image is-48x48'>
-                                <img
-                                    src='https://bulma.io/images/placeholders/96x96.png'
-                                    alt='Placeholder image'
-                                />
-                            </figure>
-                        </div>
                         <div className='media-content'>
-                            <p className='title is-4'>John Smith</p>
-                            <p className='subtitle is-6'>@johnsmith</p>
+                            <p className='title is-4'>{name}</p>
+                            <p className='subtitle is-6'>{category}</p>
                         </div>
                     </div>
 
-                    <div className='content'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                        <a href='#'>#css</a> <a href='#'>#responsive</a>
-                        <br />
-                    </div>
+                    <div className='content'>{desc}</div>
                 </div>
             </div>
         </div>
